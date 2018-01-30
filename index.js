413c8dd623561cd3aa85c679d031f230647341ad
Mark Arce <mark.arce@gmail.com>
2018-01-29 18:28:50 -0800

    Some server logic added, testing with Koa started + Added server dummy data file to explore shape of data + Built out routes and started pseudocoding logic + Server tested with example functionality only so far

---
+++ index.js
@@ -0,0 +1,70 @@
+//Koa server for client-facing API
+const route = require('koa-route');
+const Koa = require('koa');
+const app = new Koa();
+
+const port = 5000;
+
+const db = {
+  Dillon: {name: 'Dillon', laptop: 'Apple Macbook Air'},
+  Nick: {name: 'Nick', laptop: 'Dell Laptop'},
+  Jackie: {name: 'Jackie', laptop: 'Lenovo ThinkPad'}
+}
+
+const group = {
+  list: (ctx) => {
+    const names = Object.keys(db);
+    ctx.body = 'group: ' + names.join(', ');
+  },
+
+  computer: (ctx, name) => {
+    const member = db[name];
+    if (!member) {
+      return ctx.throw('Cannot find that group member', 404);
+    }
+    ctx.body = `${member.name} has a ${member.laptop}.`
+  }
+
+}
+
+const rider = {
+  signOn: (ctx, rider) => {
+    const location = rider.location;
+    const userId = rider.id;
+    //rider signs on and posts info to server
+    //generate new session object and send session ID in response
+    //fill session object with data
+    //send session object to /location/signOn
+    //store session obj in redis as ID: obj
+  },
+  destination: (ctx, rider, destination) => {
+    const location = rider.location
+    const userId = rider.id;
+    //read sessionId from redis database and extract session Obj
+    //copy location info into destination field of obj
+    //send sessionId and location to location/destination
+
+  },
+  accept: (ctx, rider) => {
+    const userId = rider.id;
+    const isBooking = rider.booking;
+  },
+  cancel: (ctx, rider) => {
+    const userId = rider.id;
+    const isBooking = rider.booking
+
+  }
+}
+
+app.use(route.get('/group', group.list));
+app.use(route.get('/group/:name', group.computer));
+/* real routes go here */
+app.use(route.post('ui/:rider/signon', rider.signOn));
+app.use(route.post('ui/:rider/:destination', rider.destination))
+app.use(route.post('ui/:rider/accept', rider.accept));
+app.use(route.post('ui/:rider/cancel'), rider.cancel);
+
+
+app.listen(port, () => {
+  console.log(`Server listening on https://localhost/${port}`)
+});
---
+++ serverDummyData.js
@@ -0,0 +1,134 @@
+// Dummy data until database is setup and working with Koa
+
+var Riders = {
+{'eWRhpRV': {
+  sessionId: eWRhpRV
+  isComplete: false
+  sessionStart: 'Sat Jan 27 2018 13:51:12 GMT-0800 (PST)'
+  sessionEnd: 'Sat Jan 27 2018 13:55:12 GMT-0800 (PST)'
+  surgeMulti: 1.0
+  price: 7.34
+  location: {
+    currentLocation: [x, y]
+    dropoffLocation: [x, y]
+  }
+  rider: {
+    riderId: number
+    firstName: string
+    picture: string
+    billingInfo: 
+  }
+  driver: {
+    driver_id: string
+    firstName: string
+    location: {
+      currentLocation: [x, y]
+      destination: [x, y]
+    }
+  }  
+},
+{'23TplPdS': {
+  sessionId: string
+  isComplete: boolean
+  sessionStart: string (date)
+  sessionEnd: string (date)
+  surgeMulti: float
+  price: float
+  location: {
+    pickupLocation: [x, y]
+    dropoffLocation: [x, y]
+  }
+  rider: {
+    riderId: number
+    firstName: string
+    picture: string
+    billingInfo: 
+  }
+  driver: {
+    driver_id: string
+    firstName: string
+    location: {
+      currentLocation: [x, y]
+      destination: [x, y]
+    }
+  }  
+},
+{userSession: {
+  sessionId: string
+  isComplete: boolean
+  sessionStart: string (date)
+  sessionEnd: string (date)
+  surgeMulti: float
+  price: float
+  location: {
+    pickupLocation: [x, y]
+    dropoffLocation: [x, y]
+  }
+  rider: {
+    riderId: number
+    firstName: string
+    picture: string
+    billingInfo: 
+  }
+  driver: {
+    driver_id: string
+    firstName: string
+    location: {
+      currentLocation: [x, y]
+      destination: [x, y]
+    }
+  }  
+},
+{userSession: {
+  sessionId: string
+  isComplete: boolean
+  sessionStart: string (date)
+  sessionEnd: string (date)
+  surgeMulti: float
+  price: float
+  location: {
+    pickupLocation: [x, y]
+    dropoffLocation: [x, y]
+  }
+  rider: {
+    riderId: number
+    firstName: string
+    picture: string
+    billingInfo: 
+  }
+  driver: {
+    driver_id: string
+    firstName: string
+    location: {
+      currentLocation: [x, y]
+      destination: [x, y]
+    }
+  }  
+},
+{userSession: {
+  sessionId: string
+  isComplete: boolean
+  sessionStart: string (date)
+  sessionEnd: string (date)
+  surgeMulti: float
+  price: float
+  location: {
+    pickupLocation: [x, y]
+    dropoffLocation: [x, y]
+  }
+  rider: {
+    riderId: number
+    firstName: string
+    picture: string
+    billingInfo: 
+  }
+  driver: {
+    driver_id: string
+    firstName: string
+    location: {
+      currentLocation: [x, y]
+      destination: [x, y]
+    }
+  }  
+}
+}
