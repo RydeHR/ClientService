5b14648057a63273df898c87cc294e6334e47954
Mark Arce <mark.arce@gmail.com>
2018-02-04 16:22:50 -0800

    Configured .env file with dotenv for environment variables / API keys Installed and configured New Relic Minor changes to data generator script Removed commented out code for initial testing of Koa

--- dataGenerator.js
+++ dataGenerator.js
@@ -135,3 +135,9 @@ var writeDataChunks = function(chunks) {
 };
 
 writeDataChunks(10);
+
+/*
+COPY rides (event_ID, event_Start, event_End, event_isClosed, rider_ID, rider_Name, driver_ID, driver_Name, driver_Is_Available, timestamp_Pickup, timestamp_Dropoff, geolocation_Pickup, geolocation_Dropoff, surgeZone, surge_Multi, price, success) FROM '/usr/local/Cellar/cassandra/3.11.1/bin/dataOutput0.csv' with delimiter =',';
+
+create table rides(event_ID uuid, event_Start timestamp, event_End timestamp, event_isClosed boolean, rider_ID int, rider_Name text, driver_ID int, driver_Name text, driver_Is_Available boolean, timestamp_Pickup timestamp, timestamp_Dropoff timestamp, geolocation_Pickup list <float>, geolocation_Dropoff list <float>, surgeZone int, surge_Multi float, price decimal, success boolean, primary key(rider_ID, event_ID));
+*/
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //Koa server for client-facing API
+const newRelic = require('newrelic');
+require('dotenv').config();
+const config = require('./config')
 const route = require('koa-route');
 const faker = require('faker');
 const fs = require('fs');
@@ -8,7 +11,7 @@ const cassandra = require('cassandra-driver');
 const uuidv4 = require('uuid/v4');
 
 /*-- Connecting to cassandra database on localhost --*/
-const client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'rydeclient'})
+const client = new cassandra.Client({contactPoints: [config.database_url], keyspace: config.database_keyspace})
 client.connect(function (err) {
   if(err) {
     console.log(err);
@@ -16,34 +19,7 @@ client.connect(function (err) {
 });
 
 /*-- Setting port for Koa server --*/
-const port = 5000;
-
-/*-- Testing Koa with details below, will remove for final implementation --*/
-
-// const db = {
-//   Dillon: {name: 'Dillon', laptop: 'Apple Macbook Pro'},
-//   Jackie: {name: 'Jackie', laptop: 'Lenovo ThinkPad'},
-//   Mark: {name: 'Nick', laptop: 'Apple Macbook Pro'},
-//   Nick: {name: 'Nick', laptop: 'Dell Laptop'}
-// }
-
-// const group = {
-//   list: (ctx) => {
-//     const names = Object.keys(db);
-//     ctx.body = 'group: ' + names.join(', ');
-//   },
-
-//   computer: (ctx, name) => {
-//     const member = db[name];
-//     if (!member) {
-//       return ctx.throw('Cannot find that group member', 404);
-//     }
-//     ctx.body = `${member.name} has a ${member.laptop}.`
-//   }
-
-// }
-
-/*-- End Koa testing details --*/
+const port = config.serverPort;
 
 //rider obj: {id: int:9, name: "string", location: [lat, long]}
 
@@ -75,41 +51,42 @@ const rider = {
     riderObj = JSON.parse(rider);
     var event = new Event(riderObj);
     event.eventStart = Date.parse(now);
+    ctx.response.code = 201;
     ctx.response.body = event.eventId;
     console.log('event stored in DBs, sent to services: ', event);
 
     //rider signs on and posts info to server
-    //generate new session object and send session ID in response
-    //fill session object with data
-    //send session object to /location/signOn
-    //store session obj in redis as ID: obj
+    //generate new event object and send event ID in response
+    //fill event object with data
+    //send event object to /location/signon
+    //store event obj in redis as ID: obj
   },
   destination: (ctx, event, destination) => {
     const eventId = event.id
-    const riderId = session.riderId;
-    //read sessionId from redis database and extract session Obj
+    const riderId = event.riderId;
+    //read eventId from redis database and extract event Obj
     //copy location info into destination field of obj
-    //send sessionId and location to location/destination
+    //send eventId and location to location/destination
 
   },
-  accept: (ctx, session) => {
+  accept: (ctx, event) => {
     //rider clicks booking button
-    const riderId = session.riderId;
-    const isBooking = session.success;
+    const riderId = event.riderId;
+    const isBooking = event.success;
 
   },
-  cancel: (ctx, session) => {
+  cancel: (ctx, event) => {
     //rider cancels instead of books at any point in transaction
-    const riderId = session.riderId;
-    const isBooking = session.success
+    const riderId = event.riderId;
+    const isBooking = event.success
 
   },
-  ridehistory: (ctx, session) => {
+  ridehistory: (ctx, event) => {
     // console.log('logged');
-    const sessionId = session;
+    const eventId = event;
     const riderId = '111111111';
 
-    let query = 'SELECT * FROM sessions WHERE rider_id = 111111111';
+    let query = 'SELECT * FROM rides WHERE rider_id = 111111111';
     return new Promise ((resolve, reject) => {
       client.execute(query, (err, results) => {
         if (err) { reject(err); }
@@ -121,15 +98,13 @@ const rider = {
   }
 }
 
-/*-- Koa testing routes below --*/
-// app.use(route.get('/group', group.list));
-// app.use(route.get('/group/:name', group.computer));
-/*-- Koa testing routes end --*/
 app.use(route.post('/ui/:rider/signon', rider.signon));
-app.use(route.post('/ui/:session/:destination', rider.destination))
-app.use(route.post('/ui/:session/accept', rider.accept));
-app.use(route.post('/ui/:session/cancel', rider.cancel));
-app.use(route.get('/ui/:session/ridehistory', rider.ridehistory));
+app.use(route.post('/ui/:event/:destination', rider.destination))
+app.use(route.post('/ui/:event/accept', rider.accept));
+app.use(route.post('/ui/:event/cancel', rider.cancel));
+app.use(route.get('/ui/:event/ridehistory', rider.ridehistory));
+
+
 
 app.listen(port, () => {
   console.log(`Server listening on https://localhost/${port}`)
